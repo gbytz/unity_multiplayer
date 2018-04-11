60e52327751eff85bd71eaae593f6ceb1863a616
Mark <stauber@stanford.edu>
2018-04-10 21:23:26 -0400

    basic arkit, jido

---
+++ Assets/JidoMaps/Editor/XcodePostprocess.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class XcodePostprocess 
+{   
+	[PostProcessBuild]
+	public static void OnPostprocessBuild (BuildTarget buildTarget, string path) {
+
+		if (buildTarget == BuildTarget.iOS) {
+			foreach (string appFilePath in AppControllerFilePaths)
+				CopyAndReplaceFile (appFilePath, Path.Combine (Path.Combine (path, "Classes/"), Path.GetFileName (appFilePath)));
+
+			foreach (string podFilePath in PodFilePaths)
+				CopyAndReplaceFile (podFilePath, Path.Combine (path, Path.GetFileName (podFilePath)));
+		}
+	}
+
+	static string OpenPodsFileName {
+		get {
+			return "open_pods.command";
+		}
+	}
+
+	internal static void CopyAndReplaceFile (string srcPath, string dstPath)
+	{
+		if (File.Exists (dstPath))
+			File.Delete (dstPath);
+
+		File.Copy (srcPath, dstPath);
+	}
+
+	static string[] PodFilePaths {
+		get {
+			return new [] {
+				Path.Combine (PodFolderPath, "Podfile"),
+				Path.Combine (PodFolderPath, "MapsyncLib.podspec"),
+				Path.Combine (PodFolderPath, "pods.command"),
+				Path.Combine (PodFolderPath, OpenPodsFileName)
+			};
+		}
+	}
+
+	static string[] AppControllerFilePaths {
+		get {
+			return new [] {
+				Path.Combine (XCodeFilesFolderPath, "MapsyncWrapper.h"),
+				Path.Combine (XCodeFilesFolderPath, "MapsyncWrapper.m")
+			};
+		}
+	}
+
+	static string PodFolderPath {
+		get {
+			return Path.Combine (XCodeFilesFolderPath, "Pod/");
+		}
+	}
+
+	static string XCodeFilesFolderPath {
+		get {
+			return Path.Combine (UnityProjectRootFolder, "Assets/JidoMaps/XCodeFiles/");
+		}
+	}
+
+	static string UnityProjectRootFolder {
+		get {
+			return ".";
+		}
+	}
+}
---
+++ Assets/JidoMaps/MapSession.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+public class MapSession : MonoBehaviour {
+	public string DeveloperKey;
+
+	private UnityMapsyncLibNativeInterface mapsyncInterface = null;
+	public MapMode Mode;
+
+	public delegate void StatusDelegate(MapStatus status);
+	public StatusDelegate StatusChangedEvent;
+
+	public delegate void AssetDelegate(MapAsset asset);
+	public AssetDelegate AssetLoadedEvent;
+
+	public delegate void BoolDelegate(bool value);
+	public BoolDelegate AssetStoredEvent;
+
+	public void Init(MapMode mapMode, string userId, string mapId) {
+		if (mapsyncInterface != null) {
+			Debug.Log ("Warning: Mapsync has already been initialized and cannot be initialized again.");
+			return;
+		}
+
+		this.Mode = mapMode;
+
+		if (string.IsNullOrEmpty (DeveloperKey)) {
+			throw new ArgumentException ("Developer Key Required");
+		}
+
+		mapsyncInterface = new UnityMapsyncLibNativeInterface(mapId, userId, DeveloperKey, mapMode == MapMode.MapModeMapping);
+	}
+
+	public void StorePlacements(List<MapAsset> assets) {
+		mapsyncInterface.SaveAssets (assets);
+	}
+
+	private void AssetReloaded(string assetJson) {
+		MapAssets assets = JsonUtility.FromJson<MapAssets> (assetJson);
+		foreach (MapAsset asset in assets.Assets) {
+			AssetLoadedEvent (asset);
+		}
+	}
+
+	private void StatusUpdated(string status) {
+		int asInt = int.Parse (status);
+		StatusChangedEvent ((MapStatus)asInt);
+	}
+
+	private void PlacementStored(string stored) {
+		AssetStoredEvent (stored == "1");
+	}
+
+}
